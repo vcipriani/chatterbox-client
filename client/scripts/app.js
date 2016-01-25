da92dd9102128b29d44218b73f6c3ef35763d743
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-01-25 14:28:50 -0800

    Explore the problem

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,28 @@
 // YOUR CODE HERE:
+var app = {};
+app.init = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function(data) {
+      var $messages = $('.messages');
+      _.each(data.results, function(messageObject) {
+        $messages.append(newMessage(messageObject.text));
+      });
+    },
+    error: function(err) {
+      console.error('bummer, you got an error. here\'s what went wrong:');
+      console.error(err);
+    }
+  });
+};
 
+$(document).on('ready', function() {
+  app.init();
+});
+
+function newMessage(text) {
+  var $node = $('<p></p>');
+  $node.text(text);
+  return $node;
+}
