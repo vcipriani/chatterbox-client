78b4dce81be34ad9c36082f424a109db84ebbbf3
vcipriani <vince.cipriani@gmail.com>
2016-01-25 16:04:07 -0800

    implement clearMessages and addMessage.  refactor send to accept a message object.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,33 +1,41 @@
 // YOUR CODE HERE:
 var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.$messages = null;
+
 app.init = function() {
 
   var self = this;
+
+  //Send message listener
   $('.submitChat').on('submit', function(event){
     event.preventDefault();
     var message = $('.newMessage').val();
     if(message==='' || message === ' ') {
       return;
     }
-    self.send(message);
+    self.send({
+      username: window.location.search,
+      text: message,
+      roomname: 'lobby'
+    });
   });  
 
+  app.$messages = $('#chats');
+  
   this.fetch();
+  setInterval(this.fetch, 10000);
   $('.newMessage').focus();
 };
 
 
-app.send = function(message, roomName) {
-  roomName = roomName || 'lobby';
-  var msg = {username: 'test',
-            text: message,
-            roomname: roomName};
+app.send = function(messageObj) {
 
-  $.ajax({url: 'https://api.parse.com/1/classes/chatterbox',
+  $.ajax({url: app.server,
         type: 'POST',
-        data: JSON.stringify(msg),
+        data: JSON.stringify(messageObj),
         success: function(){
-          console.log('Success' + msg);
+          // console.log('Success' + msg);
           $('.newMessage').val('');
           $('.newMessage').focus();
           app.fetch();
@@ -37,14 +45,13 @@ app.send = function(message, roomName) {
 
 app.fetch = function () {
   $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: app.server,
     type: 'GET',
     data: 'order=-createdAt',
     success: function(data) {
-      var $messages = $('.messages');
-      $messages.html('');
+      app.clearMessages();
       _.each(data.results, function(messageObject) {
-        $messages.append(newMessage(messageObject.text));
+        app.addMessage(messageObject);
       });
     },
     error: function(err) {
@@ -54,13 +61,19 @@ app.fetch = function () {
   });
 };
 
+app.clearMessages = function() {
+  app.$messages.html('');
+};
+
+app.addMessage = function(inputObject) {
+  var $node = $('<p></p>');
+  $node.text(inputObject.text);
+  app.$messages.append($node);
+};
+
+app.addRoom = function() {
+
+};
 $(document).on('ready', function() {
   app.init();
 });
-
-// Helper functions
-function newMessage(text) {
-  var $node = $('<p></p>');
-  $node.text(text);
-  return $node;
-}
