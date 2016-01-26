511e7dd6c867747e439c0d2946128df8f60eb97a
vcipriani <vince.cipriani@gmail.com>
2016-01-25 17:13:28 -0800

    populate room dropdown and add HTML/css formatting

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,7 +2,8 @@
 var app = {};
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 app.$messages = null;
-
+app.rooms = [];
+app.currentRoom = 'all';
 app.init = function() {
 
   var self = this;
@@ -15,7 +16,7 @@ app.init = function() {
       return;
     }
     self.send({
-      username: window.location.search,
+      username: GetURLParameter('username'),
       text: message,
       roomname: 'lobby'
     });
@@ -47,17 +48,35 @@ app.fetch = function () {
   $.ajax({
     url: app.server,
     type: 'GET',
-    data: 'order=-createdAt',
+    data: {order: '-createdAt'},
     success: function(data) {
       app.clearMessages();
       _.each(data.results, function(messageObject) {
+        if (app.rooms.indexOf(messageObject.roomname) === -1) {
+          app.rooms.push(messageObject.roomname);
+        }
         app.addMessage(messageObject);
       });
+
+
+      app.populateRoomDropdown();
     },
     error: function(err) {
       console.error('bummer, you got an error. here\'s what went wrong:');
       console.error(err);
     }
+  });
+
+};
+
+app.populateRoomDropdown = function() {
+  $('.roomSelector').html('');
+  $('.roomSelector').append('<option value="all">Choose a room..</option>');
+  app.rooms.forEach(function(item){
+    $node = $('<option></option>');
+    $node.text(item);
+    $node.val(item);
+    $('.roomSelector').append($node);
   });
 };
 
@@ -66,9 +85,14 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(inputObject) {
-  var $node = $('<p></p>');
-  $node.text(inputObject.text);
-  app.$messages.append($node);
+
+  var $chat = $('<div class="chat"></div>');
+
+  $chat.append($('<p class="username"></p>').text(inputObject.username));
+
+  $chat.append($('<p class="chat"></p>').text(inputObject.text));
+
+  app.$messages.append($chat);
 };
 
 app.addRoom = function() {
@@ -77,3 +101,15 @@ app.addRoom = function() {
 $(document).on('ready', function() {
   app.init();
 });
+
+//Helper functions
+function GetURLParameter(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) {
+        return sParameterName[1];
+    }
+  }
+}
